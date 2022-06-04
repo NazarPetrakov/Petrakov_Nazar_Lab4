e02c9aa03cd6cf84a2f9a8538d8643320ff72d99
NazarPetrakov <nazarpetrakov1@gmail.com>
2022-06-04 22:33:33 +0300

    commit

---
+++ Laboratorna4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Laboratorna4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dist = 520;
+            Garage garage = new Garage();
+
+            Console.WriteLine("Вiдстань:{0:F1} км\n", dist);
+            
+            Car BMW = new Car(150, 7,"Car BMW X5 2018", "diesel", 1000); 
+            
+            Console.WriteLine("Час:{0:F2} год",BMW.TravelTime(dist, BMW.Speed));
+            Console.WriteLine("Цiна:{0:F3} грн\n",BMW.CalcPrice(BMW.FuelType, BMW.ConsPer100,dist));
+            garage.AddVehicle(BMW);
+
+
+            Bike bike = new Bike(30,"Bike Hammer s200", 30);
+
+            Console.WriteLine("Час:{0:F2} год", bike.TravelTime(dist, bike.Speed));
+            Console.WriteLine("Цiна:{0:F3} грн\n", bike.CalcPrice(bike.FuelType, bike.ConsPer100, dist));
+            garage.AddVehicle(bike);
+
+            garage.ShowVehicle();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Laboratorna4/Vehicle.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Laboratorna4
+{
+    abstract class Vehicle
+    {
+        public int Speed { get; set; }
+        public double ConsPer100 { get; set; }
+        public string Name { get; set; }
+        public string FuelType { get; set; }
+        public double Baggage { get; set; }
+
+        public Vehicle(int Speed,string Name)
+         {
+            this.Speed = Speed;
+            this.Name = Name;
+            Console.WriteLine(Name);
+         }
+       
+        public double TravelTime(double dist, int Speed)
+        {
+             return dist / Speed; 
+        }
+       
+        public double CalcPrice(string fuel, double ConsPer100, double dist)
+         {
+            if (fuel.Equals("None"))
+            {
+                return 0;
+            }
+            if (fuel.Equals("diesel"))
+            {
+                return (ConsPer100 * dist * 55) / 100;
+            }
+            if (fuel.Equals("petrol"))
+            {
+                return (ConsPer100 * dist * 50) / 100;
+            }
+            return 0;
+         }
+
+    }
+
+    class Garage
+    {
+        List<Vehicle> vehicles = new List<Vehicle>();
+        public void AddVehicle(Vehicle vehicle)
+        {
+            vehicles.Add(vehicle);
+        }
+        public void ShowVehicle()
+        {
+            Console.WriteLine("В вашому гаражi:");
+            foreach (Vehicle vi in vehicles)
+            {
+                Console.WriteLine(vi.Name);
+            }
+        }
+
+    }
+
+    class Car:Vehicle
+    {
+        
+        public Car(int Speed, double ConsPer100, string Name, string FuelType, double Baggage) : base(Speed, Name)
+        {
+            this.Baggage = Baggage;
+            this.FuelType = FuelType;
+            this.ConsPer100 = ConsPer100;
+        }      
+        
+    }
+    class Bike : Vehicle
+    {
+        public Bike(int Speed, string Name, double Baggage) : base(Speed, Name)
+        {
+            this.Baggage = Baggage;
+            FuelType = "None";
+            ConsPer100 = 0;
+        }
+       
+    }
+    class Wagon : Vehicle
+    {
+        public Wagon(int Speed, string Name, double Baggage) : base(Speed, Name)
+        {
+            this.Baggage = Baggage;
+            FuelType = "None";
+            ConsPer100 = 0;
+        }
+    }
+    class Unicycle : Vehicle
+    {
+        public Unicycle(int Speed, string Name) : base(Speed, Name)
+        {
+            FuelType = "None";
+            ConsPer100 = 0;
+            Baggage = 0;
+        }
+        
+    }
+
+}
